4d9e1266728368d581790cd18685578280266a4d
Dennis Empirio <dennis.empirio@gmail.com>
2023-01-26 01:34:46 +0100

    commit 1

--- index.php
+++ index.php
@@ -1,3 +1,20 @@
 <?php
-
+    class Product{
+        protected $price; //proprietà per il prezzo del prodotto
+    
+        public function __construct($price)
+        {
+            $this->setPrice($price);   
+        }
+        public function setPrice($price){
+            $this->price = $price; // impostare il prezzo del prodotto
+        }
+        public function getPrice(){
+            return $this->price; //recuperare il prezzo del prodotto
+        }
+        public function getHtml(){
+            echo '<div>' . $this->price . ' €</div>'; //stampa del prezzo
+        }
+    }
+    
 ?>
